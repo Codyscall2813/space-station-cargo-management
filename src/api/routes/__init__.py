7a6420883f42c4e22c4b19e95a63a24328744304
Himanshu <himanshucodys.work@gmail.com>
2025-05-02 22:22:55 +0530

    Delete src directory

--- src/api/routes/__init__.py
+++
@@ -1,2 +0,0 @@
-# Import route modules
-from src.api.routes import placement, retrieval, simulation, waste_management, import_export, logs
--- src/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from src.db.session import Base
-from src.models.container import Container
-from src.models.item import Item
-from src.models.position import Position
-from src.models.log import LogEntry, ActionType
-from src.models.return_mission import ReturnMission, WasteItem
